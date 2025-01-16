ab551798051baa41dd88d5febc83011ea6a8ffa2
bonato <lucasbon4to@gmail.com>
2025-01-16 17:21:29 -0300

    add runQuery method

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
@@ -21,8 +22,21 @@ func main() {
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 
-	dsn := dsn_builder(dbHost, dbPort, dbUser, dbPassword)
-	fmt.Println("dsn configurada:", dsn)
+	PrintTime("Building DSN...")
+	dsn := dsnBuilder(dbHost, dbPort, dbUser, dbPassword)
+
+	PrintTime("Connecting to Database...")
+	db, err := sql.Open("go_ibm_db", dsn)
+	if err != nil {
+		log.Fatal("Unable to connect to Database:", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error while testing connection with Database:", err)
+	}
+	PrintTime("Connection established.")
 }
 
 // imprime o log com timestamp
@@ -35,4 +49,51 @@ func PrintTime(text string) {
 func dsnBuilder(dbHost string, dbPort string, dbUser string, dbPassword string) string {
 	dsn := "HOSTNAME=" + dbHost + ";PORT=" + dbPort + ";DATABASE=HUGOPIET;UID=" + dbUser + ";PWD=" + dbPassword
 	return dsn
+}
+
+func runQuery(db *sql.DB, query string) {
+	start := time.Now()
+
+	// processa query
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal("Error while processing query:", err)
+	}
+	defer rows.Close()
+
+	// busca nome das colunas
+	columns, err := rows.Columns()
+	if err != nil {
+		log.Fatal("Error while getting column names:", err)
+	}
+
+	for rows.Next() {
+		// cria um slice de interfaces para armazenar os valores
+		values := make([]interface{}, len(columns))
+		valuePtrs := make([]interface{}, len(columns))
+		for i := range values {
+			valuePtrs[i] = &values[i]
+		}
+
+		// lê a linha
+		err := rows.Scan(valuePtrs...)
+		if err != nil {
+			log.Fatal("Error while scanning results: ", err)
+		}
+
+		// imprime os valores
+		// result := make(map[string]interface{})
+		// for i, col := range columns {
+		// 	val := values[i]
+		// 	result[col] = val
+		// }
+		// fmt.Println(result)
+	}
+	// verifica por erros na iteração
+	if err = rows.Err(); err != nil {
+		log.Fatal("Error while iterating: ", err)
+	}
+
+	// imprime o tempo de execução
+	fmt.Printf("Query processed in %s\n", time.Since(start))
 }
