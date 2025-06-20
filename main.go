ffb3c057204c4f2e906ba2362a2edebaa606b40f
Lucas G. Bonato <lucasbon4to@gmail.com>
2025-06-20 17:40:30 -0300

    add consistent logging method
    add consistent logging method over github projects

--- main.go
+++ main.go
@@ -35,6 +35,10 @@ func main() {
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
+func logRequest(level string, status int, r *http.Request, source int) {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	ip := r.RemoteAddr
+	log.Printf("[%s] (%s) %d | %s (%d - %s)", level, timestamp, status, ip, source, r.URL.Path)
 }
 
 // queryHandler handles incoming HTTP POST requests to /query
